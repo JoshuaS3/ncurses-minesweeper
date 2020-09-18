63b24cb6c475d7b320d0b123519317a2163fcd5d
Josh Stockin <joshstockin@gmail.com>
2020-09-17 22:41:59 -0500

    Fix copyright disclaimer

--- src/draw/title.c
+++ src/draw/title.c
@@ -1,6 +1,6 @@
 /* ncurses-minesweeper Copyright (c) 2020 Joshua 'joshuas3' Stockin
  * <https://joshstock.in>
- * <https://github.com/JoshuaS3/lognestmonster>
+ * <https://github.com/JoshuaS3/ncurses-minesweeper>
  *
  * This software is licensed and distributed under the terms of the MIT License.
  * See the MIT License in the LICENSE file of this project's root folder.
